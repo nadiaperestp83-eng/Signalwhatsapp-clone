0ff6f5e8e89c754cdb34fe2b888a227b513ac855
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-08-26 06:48:48 -0300

    Update body.dart

--- lib/screens/signup/components/body.dart
+++ lib/screens/signup/components/body.dart
@@ -1,15 +1,19 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/screens/signup/components/profile_picture.dart';
 import 'package:whatsapp_clone/screens/signup/components/username_input_field.dart';
-
 
 class SignupScreenBody extends StatelessWidget {
   const SignupScreenBody({
     super.key,
     required this.usernameController,
+    required this.avatarSelecionado,
+    required this.onAvatarSelecionado,
   });
 
   final TextEditingController usernameController;
+  final File? avatarSelecionado;
+  final ValueChanged<File?> onAvatarSelecionado;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +22,14 @@ class SignupScreenBody extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: MediaQuery.of(context).size.height * .08),
-            const UserProfilePicture(),
-            UsernameInputField(usernameController: usernameController)
+            UserProfilePicture(
+              avatarSelecionado: avatarSelecionado,
+              onAvatarSelecionado: onAvatarSelecionado,
+            ),
+            UsernameInputField(
+              usernameController: usernameController,
+              avatarSelecionado: avatarSelecionado,
+            ),
           ],
         ),
       ),
